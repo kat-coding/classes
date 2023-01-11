041abd4ee6af2e7b812acac3a33b3af82a1ed071
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-01-10 19:47:55 -0500

    finished linkedIn classes

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+'use strict'; //automatic in classes
+let mixin = {
+    madeIn(){
+        console.log('This car was made this year.');
+    }
+}
+let carMixin = {
+    __proto__: mixin,
+
+    madeIn() {
+        super.madeIn();
+    }
+}
+
 class Car{
+//classes are not hoisted
     //properties
     constructor(doors, engine, color) {
         this.doors = doors;
@@ -9,8 +24,49 @@ class Car{
     carStats(){
         return `This car has ${this.doors} doors, a ${this.engine} engine, and is ${this.color}.`;
     }
-}
 
-const cx5 = new Car(4, 'V6', 'red');
-console.log(cx5);
-console.log(cx5.carStats());
+    //static method example
+    static totalDoors(car1, car2){
+        const doors1 = car1.doors;
+        const doors2 = car2.doors;
+        return doors1 + doors2;
+    }
+}
+class SUV extends Car {
+    constructor(doors, engine, color, carStats) {
+        super(doors, engine, color, carStats);
+        this._brand = 'no brand yet';
+        this.wheels = 4;
+        this.ac = true;
+
+        //assign mixin inside constructor
+        Object.assign(this, carMixin);
+    }
+    get getBrand(){
+        return this._brand;
+    }
+    set setBrand(newBrand){
+        this._brand = newBrand;
+    }
+    myBrand(){
+        return console.log(`This SUV is a ${this._brand}.`)
+    }
+}
+//function hoisting
+// sayHi();
+
+const cx5 = new SUV(4, 'V6', 'red');
+// const civic = new Car(3, 'V4', 'green');
+console.log(cx5.getBrand);
+cx5.setBrand = 'Mazda';
+console.log(cx5.getBrand);
+console.log(cx5.madeIn());
+// console.log(civic);
+// console.log(civic.carStats());
+//
+// //calling the static method using the Car class
+// console.log(Car.totalDoors(cx5, civic));
+//
+// function sayHi(){
+//     return console.log("This function can be called anywhere.");
+// }
